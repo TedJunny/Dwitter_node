4ec358d66d63d7fd2d0d9a43d6998ba7c8a1491c
YeJun <hayejun1013@naver.com>
2021-10-12 14:22:54 +0900

    feat: add CRUD for tweet messages

---
+++ Server/router/tweets.js
@@ -0,0 +1,74 @@
+import express from "express";
+import "express-async-errors";
+
+let tweets = [
+  {
+    id: "1",
+    text: "드림코더 화이팅",
+    createdAt: Date.now().toString(),
+    name: "Ted",
+    username: "tedjunny",
+    url: "https://i.ibb.co/jWqbYPN/IMG-0008.jpg",
+  },
+  {
+    id: "2",
+    text: "노드 너무 재미있어요",
+    createdAt: Date.now().toString(),
+    name: "MooHyun",
+    username: "moohyun",
+  },
+];
+const router = express.Router();
+
+router.get("", (req, res, next) => {
+  const username = req.query.username;
+  const data = username
+    ? tweets.filter((tweet) => tweet.username === username)
+    : tweets;
+  res.status(200).json(data);
+});
+
+router.get("/:id", (req, res, next) => {
+  const id = req.params.id;
+  const tweet = tweets.find((tweet) => tweet.id === id);
+  if (tweet) {
+    res.status(200).json(tweet);
+  } else {
+    res.status(404).json({ message: `Tweet id(${id}) not found` });
+  }
+});
+
+router.post("", (req, res, next) => {
+  const { text, name, username } = req.body;
+  const tweet = {
+    id: Date.now().toString(),
+    text,
+    createdAt: new Date(),
+    name,
+    username,
+  };
+
+  tweets = [tweet, ...tweets];
+  res.status(201).json(tweet);
+});
+// PUT
+router.put("/:id", (req, res, next) => {
+  const id = req.params.id;
+  const text = req.body.text;
+  const tweet = tweets.find((tweet) => tweet.id === id);
+  if (tweet) {
+    tweet.text = text;
+    res.status(200).json(tweet);
+  } else {
+    res.status(404).json({ message: `Tweet id(${id}) not found` });
+  }
+});
+
+// DELETE
+router.delete("/:id", (req, res, next) => {
+  const id = req.params.id;
+  tweets = tweets.filter((tweet) => tweet.id !== id);
+  res.sendStatus(204);
+});
+
+export default router;
