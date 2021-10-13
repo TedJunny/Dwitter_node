4f3c41a8db2da60bcdc0c57964ddb284f4e1cb85
YeJun <hayejun1013@naver.com>
2021-10-13 12:00:02 +0900

    fead : add validation logic on APIs

--- Server/router/tweets.js
+++ Server/router/tweets.js
@@ -1,13 +1,22 @@
 import express from "express";
 import "express-async-errors";
 import * as tweetController from "../controller/tweet.js";
+import { body } from "express-validator";
+import { validate } from "../middleware/validator.js";
 
 const router = express.Router();
+const validateTweet = [
+  body("text")
+    .trim()
+    .isLength({ min: 3 })
+    .withMessage("text should be at least 3 chracters"),
+  validate,
+];
 
 router.get("", tweetController.getAll);
-router.post("", tweetController.create);
+router.post("", validateTweet, tweetController.create);
 router.get("/:id", tweetController.getById);
-router.put("/:id", tweetController.update);
+router.put("/:id", validateTweet, tweetController.update);
 router.delete("/:id", tweetController.remove);
 
 export default router;
