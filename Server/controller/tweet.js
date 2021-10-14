2cce3db1ab4e49bff207e782faeecde9ea5a5c2e
YeJun <hayejun1013@naver.com>
2021-10-14 15:40:42 +0900

    feat: add authorize logic on PUT and DELETE method

--- Server/controller/tweet.js
+++ Server/controller/tweet.js
@@ -27,16 +27,26 @@ export async function create(req, res, next) {
 export async function update(req, res, next) {
   const id = req.params.id;
   const text = req.body.text;
-  const tweet = await tweetRepository.update(id, text);
-  if (tweet) {
-    res.status(200).json(tweet);
-  } else {
-    res.status(404).json({ message: `Tweet id(${id}) not found` });
+  const tweet = await tweetRepository.getById(id);
+  if (!tweet) {
+    return res.sendStatus(404);
   }
+  if (tweet.userId !== req.userId) {
+    return res.sendStatus(403);
+  }
+  const updated = await tweetRepository.update(id, text);
+  res.status(200).json(updated);
 }
 
 export async function remove(req, res, next) {
   const id = req.params.id;
+  const tweet = await tweetRepository.getById(id);
+  if (!tweet) {
+    return res.sendStatus(404);
+  }
+  if (tweet.userId === req.userId) {
+    return res.sendStatus(403);
+  }
   await tweetRepository.remove(id);
   res.sendStatus(204);
 }
