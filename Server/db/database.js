806a9b42cd0e43b60c8bed847dbc64875f775453
YeJun <hayejun1013@naver.com>
2021-10-17 20:02:04 +0900

    feat: import Sequelize ORM

--- Server/db/database.js
+++ Server/db/database.js
@@ -1,11 +1,18 @@
 import mysql from "mysql2";
 import { config } from "../config.js";
+import SQ from "sequelize";
+
+const { host, user, database, password } = config.db;
+export const sequelize = new SQ.Sequelize(database, user, password, {
+  host,
+  dialect: "mysql",
+});
 
 const pool = mysql.createPool({
-  host: config.db.host,
-  user: config.db.user,
-  database: config.db.database,
-  password: config.db.password,
+  host,
+  user,
+  database,
+  password,
 });
 
 export const db = pool.promise();
